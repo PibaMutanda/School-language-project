403895033a06ef57b31e7e056281d43840118f3b
Piba7 <pibamutanda@hotmail.com>
2015-04-20 23:31:34 +0200

    Participant correction

--- SchoolOfLanguage/src/main/java/be/school/model/Participant.java
+++ SchoolOfLanguage/src/main/java/be/school/model/Participant.java
@@ -23,7 +23,7 @@ import javax.validation.constraints.Pattern;
 import org.hibernate.validator.constraints.NotEmpty;
 
 @Entity
-public class Participant<E> {
+public class Participant {
 
 	@Id
 	@GeneratedValue
